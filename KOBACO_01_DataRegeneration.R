f5341643237a0da6d8f62f67c532c4d41cdcac81
Keungoui Kim <56111110+awekim@users.noreply.github.com>
2024-10-16 13:58:16 +0900

    Add files via upload

--- KOBACO_01_DataRegeneration.R
+++ KOBACO_01_DataRegeneration.R
@@ -14,8 +14,9 @@
 library(dplyr)
 library(magrittr)
 
+year <- 2022
+
 ### SWD
-year <- 2023
 temp.list <- 
   list.files(paste0("../6.김경외 연구자님/",year,"/t_tv_live_",year,"/SWD/"))
 
@@ -27,14 +28,16 @@ for(i in 1:length(temp.list)){
                         Channel = substr(V1,31,34), StartTime = substr(V1, 35,40),
                         EndTime = substr(V1, 41,46), TV= substr(V1, 47,47),
                         AudienceType=substr(V1, 48,48)) %>%
-            select(-c("V1")) %>% mutate(Date=substr(temp.list[i],3,8)))
+            select(-c("V1")) %>% mutate(Year=substr(temp.list[i],3,4),
+                                        Month=substr(temp.list[i],5,6),
+                                        Day=substr(temp.list[i],7,8)))
   print(i)
 }
 swd.df <- swd.df[2:nrow(swd.df),]
-save(swd.df, file=paste0("R file/swd.",year,".df"))
+save(swd.df, file=paste0("R file/swd.",year,".RData"))
+rm(swd.df)
 
 ### WEH
-year <- 2022
 temp.list <- 
   list.files(paste0("../6.김경외 연구자님/",year,"/t_tv_live_",year,"/DEM_WEH/"),
              pattern=".weh")
@@ -45,14 +48,15 @@ for(i in 1:length(temp.list)){
   weh.df <- weh.df %>%
     rbind(temp %>% mutate(HouseholdID = substr(V1,1,28), NWeight = substr(V1,29,30),
                           Reg = substr(V1,31,33), Weight = substr(V1, 34,41)) %>%
-            select(-c("V1")) %>% mutate(Date=substr(temp.list[i],3,8)))
+            select(-c("V1")) %>% mutate(Year=substr(temp.list[i],3,4),
+                                        Month=substr(temp.list[i],5,6),
+                                        Day=substr(temp.list[i],7,8)))
   print(i)
 }
 weh.df <- weh.df[2:nrow(weh.df),]
 save(weh.df, file=paste0("R file/weh.",year,".RData"))
 
 ### DEM
-year <- 2022
 temp.list <- 
   list.files(paste0("../6.김경외 연구자님/",year,"/t_tv_live_",year,"/DEM_WEH/"),
              pattern=".dem")
@@ -67,9 +71,12 @@ for(i in 1:length(temp.list)){
             rename(Sex=V2, Job=V3, Household=V4, Teen=V6) %>%
             mutate(Machines=substr(V5,1,1), Cable=substr(V5,2,2), 
                    SkyLife=substr(V7,1,1),IPTV=substr(V7,2,2),
-                   Date=substr(temp.list[i],3,8)) %>%
+                   Year=substr(temp.list[i],3,4),
+                   Month=substr(temp.list[i],5,6),
+                   Day=substr(temp.list[i],7,8)) %>%
             select(HouseholdID, IndividualID, IndWeight, Reg, Sex, Job,
-                   Household, Teen, Machines, Cable, SkyLife, IPTV, Date))
+                   Household, Teen, Machines, Cable, SkyLife, IPTV, 
+                   Year, Month, Day))
   
   print(i)
 }
